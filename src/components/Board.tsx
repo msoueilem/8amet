04c2c9288e4028fb8ccf0145f48ebc115526ac54
​Mohamed Soueilem <msoueilem@webmd.net>
2024-09-09 14:49:12 -0400

    dots board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import '../scss/Board.scss';
 
+const rows = 13; // Adjust based on your design
+const cols = 13; // Adjust based on your design
+
+const generateGrid = (rows: number, cols: number) => {
+  let grid = [];
+  for (let i = 0; i < rows; i++) {
+    let row = [];
+    for (let j = 0; j < cols; j++) {
+      row.push(<div className="dot" key={`dot-${i}-${j}`}></div>);
+    }
+    grid.push(<div className="row" key={`row-${i}`}>{row}</div>);
+  }
+  return grid;
+};
+
 const Board: React.FC = () => {
   return <div className='game-board'>
-    Game Board Here
+    {generateGrid(rows, cols)}
   </div>;
 };
 
